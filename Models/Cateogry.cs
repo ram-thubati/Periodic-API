263945f35f8a3559c824d4647bcb4b3d6cb6f84f
Ram <76797053+ram-thubati@users.noreply.github.com>
2022-08-15 00:20:03 -0400

    Create Category Model
    For analytics

---
+++ Models/Cateogry.cs
@@ -0,0 +1,11 @@
+namespace Periodic.Models
+{
+    public class Category
+    {
+        public int Id{get; set;}
+
+        public string MainCategory{get; set;}
+
+        public string SubCategory{get; set;}
+    }
+}
