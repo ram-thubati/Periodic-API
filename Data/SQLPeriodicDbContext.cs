bf99fe689d5c36505811668b6517261c9112822d
Ram <76797053+ram-thubati@users.noreply.github.com>
2022-08-15 00:20:26 -0400

    create DbContext

---
+++ Data/SQLPeriodicDbContext.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Periodic.Models;
+
+namespace Periodic.Data
+{
+    public class SQLPeriodicDbContext : DbContext
+    {
+        public SQLPeriodicDbContext(DbContextOptions opt) : base(opt)
+        {
+            
+        }
+
+        public DbSet<Account> Accounts {get; set;}
+
+        public DbSet<Transaction> Transactions{get; set;}
+
+        public DbSet<Scheduled> ScheduledTransactions{get; set;}
+
+        public DbSet<User> Users{get; set;}
+    }
+}
