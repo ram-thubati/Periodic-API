46f4caa0c7f6bf9625a7dd87ae7d82fb7b1f1476
Ram <76797053+ram-thubati@users.noreply.github.com>
2022-08-16 23:32:21 -0400

    added DI in program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using Periodic.Secret;
+using Periodic.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<SQLPeriodicDbContext>(options => options.UseSqlServer(Secrets.SQLConnectionString));
+
+builder.Services.AddScoped<IPeriodicRepo, SQLPeriodicRepo>();
+
 
 var app = builder.Build();
 
